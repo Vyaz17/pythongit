f17f336814d0725ccbb96570855e965596b47b5d
Vyaz17 <alexandervyazovoi@gmail.com>
2022-02-10 20:43:05 +0200

    add all files

---
+++ main.py
@@ -0,0 +1,20 @@
+# git --version
+
+# git init
+
+# 1) git status
+# 2) git add main.py
+# 3) git commit -m "add main.py"
+
+for i in range(1,5):
+    print(i)
+
+print("hello")
+
+# 4) git diff main.py
+
+print("oooooooo")
+
+# 4) git branch -M main
+# 5)git remote add origin https://github.com/Vyaz17/pythongit.git
+#6)git push -u origin main
